ab1bf8e26c1cc276b883407033aa3fcd40be1b4b
SHUBHAM01S2 <140646155+SHUBHAM01S2@users.noreply.github.com>
2024-02-24 10:59:12 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 const container = document.querySelector(".container");
 
-const seats = document.querySelectorAll(".row .seats:not(.sold)");
+const seats = document.querySelectorAll(".row .seats:not(.booked)");
+
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+
+const parkingSelect = document.getElementById("parking");
+
+let parkingPrice = +parkingSelect.value;
+console.log(parkingPrice);
+
+parkingSelect.addEventListener("change", (e) => {
+  // update parking price
+  parkingPrice = +e.target.value;
+  console.log(parkingPrice);
+});
+
+// Event Listener for Slots Click
+container.addEventListener("click", (e) => {
+  // check if a slot is clicked and not booked
+  if (
+    e.target.classList.contains("slot") &&
+    !e.target.classList.contains("booked")
+  )
+    // toggle book selection
+    e.target.classList.toggle("selected");
+    // update displayed count and total
+    updateSelectedCount();
+});
+
+
+function updateSelectedCount(){
+    
+}
